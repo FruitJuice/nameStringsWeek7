c135839ec600351ca688457a8acca047a6f26f0e
Andrius <Andrius@LAPTOP-MUQJH65O>
2016-10-17 22:33:14 +0100

    Finalthing

--- nameStringsWeek7/nameStringsWeek7.cpp
+++ nameStringsWeek7/nameStringsWeek7.cpp
@@ -2,10 +2,75 @@
 //
 
 #include "stdafx.h"
+#include <string.h>
 
 
 int main()
 {
-    return 0;
+	int i = 0;
+	int j = 0;
+	int v = 0;
+
+	char names[10][25] = { "Whitney Wright", "Vicky Collins","Essie Ray","Sammy Wade","Kara Wilson","Christie Butler","Ginger Dennis","Sean Parsons","Beatrice Simpson","Leigh Gutierrez" };
+	char firstNames[10][25];
+	char lastNames[10][25];
+	char mixedNames[10][25];
+
+	//Detect first names and puts then into string firstName
+	puts("FIRST NAMES:");
+	do
+	{
+		if (names[i][j] == ' ') //when a space is found \0 is iserted in that same space in firstName to end that string
+		{
+			firstNames[i][j] = '\0';
+			puts(firstNames[i]);
+			i++;
+			j = 0; //resetting for next string
+		}
+		else       //I increase j by one and check to see if it's a space
+		{
+			firstNames[i][j] = names[i][j];
+			j++;
+		}
+	} while (i < 10);
+
+
+
+	i = 0;
+	j = 0;
+
+	printf("\n");
+
+	//Detects the last name from names and inserts it inside lastName
+	puts("SURNAMES:");
+	for (i = 0; i < 10; i++)
+	{
+		while (names[i][j] != ' ') //checking for a space and increasing j by 1 until a space is found
+			j++;
+		while (names[i][j] != '\0')
+		{
+			lastNames[i][v] = names[i][j + 1];//storing every value after the space inside lastNames until a \0 is found
+			v++;
+			j++;
+		}
+		lastNames[i][v] = '\0'; //ending the string
+		puts(lastNames[i]);
+		v = 0;
+		j = 0;
+	}
+	printf("\n");
+
+	//Creates a new name by combining strings from firstNames and lastNames
+	puts("NEW NAMES:");
+	for (i = 0, j = 9; i <= 9; i++, j--)
+	{
+		strcpy_s(mixedNames[i], firstNames[i]); //Copy instead of Concatenate as mixedNames is empty
+		strcat_s(mixedNames[i], " ");
+		strcat_s(mixedNames[i], lastNames[j]);
+		puts(mixedNames[i]);
+	}
+
+	printf("\n");
+	return 0;
 }
 
